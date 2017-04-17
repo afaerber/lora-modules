be6f9c6b24d4af7d8ff45dc4fe943b55a881df39
Andreas Färber <afaerber@suse.de>
2017-04-17 04:14:09 +0200

    rn2483: Prepare support for RN2903 and further cleanups
    Change rn2483_readline_timeout() to return error codes. This simplifies
    the caller code.
    
    Add helpers for dealing with EUIs.
    
    Factor command handling out, and use individual helper functions for
    commands. Store model, band and HWEUI in the device.
    
    Use the model information to allow RN2903 as well.
    
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- rn2483.c
+++ rn2483.c
@@ -1,5 +1,5 @@
 /*
- * Microchip RN2483
+ * Microchip RN2483/RN2903
  *
  * Copyright (c) 2017 Andreas Färber
  *
@@ -12,9 +12,35 @@
 #include <linux/of.h>
 #include <linux/serdev.h>
 
+typedef u8 lora_eui[8];
+
+#define PRIxLORAEUI "%02x%02x%02x%02x%02x%02x%02x%02x"
+#define PRIXLORAEUI "%02X%02X%02X%02X%02X%02X%02X%02X"
+#define LORA_EUI(x) x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7]
+
+static int lora_strtoeui(const char *str, lora_eui *val)
+{
+	char buf[3];
+	int i, ret;
+
+	for (i = 0; i < 8; i++) {
+		strncpy(buf, str + i * 2, 2);
+		buf[2] = 0;
+		ret = kstrtou8(buf, 16, &(*val)[i]);
+		if (ret)
+			return ret;
+	}
+	return 0;
+}
+
+#define RN2483_CMD_TIMEOUT HZ
+
 struct rn2483_device {
 	struct serdev_device *serdev;
 	struct gpio_desc *reset_gpio;
+	unsigned model;
+	lora_eui hweui;
+	unsigned band;
 	bool saw_cr;
 	void *buf;
 	size_t buflen;
@@ -22,15 +48,79 @@ struct rn2483_device {
 	struct completion line_read_comp;
 };
 
-static char *rn2483_readline_timeout(struct rn2483_device *rndev, unsigned long timeout)
+static int rn2483_readline_timeout(struct rn2483_device *rndev, char **line, unsigned long timeout);
+
+static int rn2483_send_command_timeout(struct rn2483_device *rndev,
+	const char *cmd, char **resp, unsigned long timeout)
 {
-	char *tmp;
+	int ret;
+
+	ret = serdev_device_write_buf(rndev->serdev, cmd, strlen(cmd));
+	if (ret < 0)
+		return ret;
+
+	ret = serdev_device_write_buf(rndev->serdev, "\r\n", 2);
+	if (ret < 0)
+		return ret;
+
+	return rn2483_readline_timeout(rndev, resp, timeout);
+}
+
+static int rn2483_sys_get_hweui(struct rn2483_device *rndev, lora_eui *val)
+{
+	int ret;
+	char *line;
+
+	ret = rn2483_send_command_timeout(rndev, "sys get hweui", &line, RN2483_CMD_TIMEOUT);
+	if (ret)
+		return ret;
+
+	ret = lora_strtoeui(line, val);
+	devm_kfree(&rndev->serdev->dev, line);
+	return ret;
+}
+
+static int rn2483_mac_get_band(struct rn2483_device *rndev, uint *val)
+{
+	int ret;
+	char *line;
+
+	ret = rn2483_send_command_timeout(rndev, "mac get band", &line, RN2483_CMD_TIMEOUT);
+	if (ret)
+		return ret;
+
+	ret = kstrtouint(line, 10, val);
+	devm_kfree(&rndev->serdev->dev, line);
+
+	return ret;
+}
+
+static int rn2483_mac_get_status(struct rn2483_device *rndev, u32 *val)
+{
+	int ret;
+	char *line;
+
+	ret = rn2483_send_command_timeout(rndev, "mac get status", &line, RN2483_CMD_TIMEOUT);
+	if (ret)
+		return ret;
+
+	ret = kstrtou32(line, 16, val);
+	devm_kfree(&rndev->serdev->dev, line);
+	return ret;
+}
+
+static int rn2483_readline_timeout(struct rn2483_device *rndev, char **line, unsigned long timeout)
+{
 	timeout = wait_for_completion_timeout(&rndev->line_recv_comp, timeout);
 	if (!timeout)
-		return NULL;
-	tmp = devm_kstrdup(&rndev->serdev->dev, rndev->buf, GFP_KERNEL);
+		return -ETIMEDOUT;
+
+	*line = devm_kstrdup(&rndev->serdev->dev, rndev->buf, GFP_KERNEL);
 	complete(&rndev->line_read_comp);
-	return tmp;
+	if (!*line)
+		return -ENOMEM;
+
+	return 0;
 }
 
 static void rn2483_receive_line(struct rn2483_device *rndev, const char *sz, size_t len)
@@ -92,7 +182,9 @@ static const struct serdev_device_ops rn2483_serdev_client_ops = {
 static int rn2483_probe(struct serdev_device *sdev)
 {
 	struct rn2483_device *rndev;
-	char *line;
+	char *line, *cmd;
+	char sz[5];
+	u32 status;
 	int ret;
 
 	dev_info(&sdev->dev, "Probing");
@@ -116,11 +208,6 @@ static int rn2483_probe(struct serdev_device *sdev)
 		return ret;
 	}
 
-	if (!sdev->ctrl) {
-		dev_err(&sdev->dev, "!ctrl");
-		return -EINVAL;
-	}
-
 	serdev_device_set_baudrate(sdev, 57600);
 	serdev_device_set_flow_control(sdev, false);
 
@@ -130,32 +217,73 @@ static int rn2483_probe(struct serdev_device *sdev)
 	gpiod_set_value_cansleep(rndev->reset_gpio, 1);
 	msleep(100);
 
-	line = rn2483_readline_timeout(rndev, HZ);
-	if (!line) {
-		dev_err(&sdev->dev, "Timeout");
-		ret = -ETIMEDOUT;
+	ret = rn2483_readline_timeout(rndev, &line, HZ);
+	if (ret) {
+		if (ret != -ENOMEM)
+			dev_err(&sdev->dev, "Timeout waiting for firmware identification");
 		goto err_timeout;
 	}
 
-	if (!(strlen(line) > 7 && strncmp(line, "RN2483 ", 7) == 0)) {
+	if (strlen(line) < strlen("RNxxxx X.Y.Z MMM DD YYYY HH:MM:SS") || line[6] != ' ' ||
+			strncmp(line, "RN", 2) != 0) {
 		dev_err(&sdev->dev, "Unexpected response '%s'", line);
 		devm_kfree(&sdev->dev, line);
 		ret = -EINVAL;
 		goto err_version;
 	}
 	dev_info(&sdev->dev, "Firmware '%s'", line);
+	strncpy(sz, line + 2, 4);
+	sz[4] = 0;
 	devm_kfree(&sdev->dev, line);
+	ret = kstrtouint(sz, 10, &rndev->model);
+	if (ret)
+		goto err_model;
+	if (!(rndev->model == 2483 || rndev->model == 2903)) {
+		dev_err(&sdev->dev, "Unknown model %u", rndev->model);
+		ret = -ENOTSUPP;
+		goto err_model;
+	}
+	dev_info(&sdev->dev, "Detected RN%u", rndev->model);
 
-	serdev_device_write_buf(sdev, "sys get hweui\r\n", strlen("sys get hweui\r\n"));
-	line = rn2483_readline_timeout(rndev, HZ);
-	if (line) {
-		if (strcmp(line, "invalid_param") != 0)
-			dev_info(&sdev->dev, "HWEUI = %s", line);
+	ret = rn2483_sys_get_hweui(rndev, &rndev->hweui);
+	if (ret) {
+		if (ret != -ENOMEM)
+			dev_err(&sdev->dev, "Failed to read HWEUI (%d)", ret);
+		goto err_hweui;
+	}
+	dev_info(&sdev->dev, "HWEUI " PRIxLORAEUI, LORA_EUI(rndev->hweui));
+
+	switch (rndev->model) {
+	case 2483:
+		ret = rn2483_mac_get_band(rndev, &rndev->band);
+		if (ret) {
+			dev_err(&sdev->dev, "Failed to read band (%d)", ret);
+			goto err_band;
+		}
+		dev_info(&sdev->dev, "Frequency band %u", rndev->band);
+		break;
+	case 2903:
+		/* No "mac get band" command available */
+		rndev->band = 915;
+		break;
+	}
+
+	ret = rn2483_mac_get_status(rndev, &status);
+	if (!ret)
+		dev_info(&sdev->dev, "MAC status %08x", status);
+
+	cmd = "mac get sync";
+	ret = rn2483_send_command_timeout(rndev, cmd, &line, HZ);
+	if (!ret) {
+		dev_info(&sdev->dev, "%s => '%s'", cmd, line);
 		devm_kfree(&sdev->dev, line);
 	}
 
 	return 0;
 
+err_band:
+err_hweui:
+err_model:
 err_version:
 err_timeout:
 	gpiod_set_value_cansleep(rndev->reset_gpio, 0);
@@ -177,6 +305,7 @@ static void rn2483_remove(struct serdev_device *sdev)
 
 static const struct of_device_id rn2483_of_match[] = {
 	{ .compatible = "microchip,rn2483" },
+	{ .compatible = "microchip,rn2903" },
 	{}
 };
 MODULE_DEVICE_TABLE(of, rn2483_of_match);
