773c9727c2ddc7faba876c8b88815a3c0c9c5a93
Andreas Färber <afaerber@suse.de>
2017-04-17 22:44:13 +0200

    rn2483: Move EUI helpers to generic header
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- rn2483.c
+++ rn2483.c
@@ -12,26 +12,7 @@
 #include <linux/of.h>
 #include <linux/serdev.h>
 
-typedef u8 lora_eui[8];
-
-#define PRIxLORAEUI "%02x%02x%02x%02x%02x%02x%02x%02x"
-#define PRIXLORAEUI "%02X%02X%02X%02X%02X%02X%02X%02X"
-#define LORA_EUI(x) x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7]
-
-static int lora_strtoeui(const char *str, lora_eui *val)
-{
-	char buf[3];
-	int i, ret;
-
-	for (i = 0; i < 8; i++) {
-		strncpy(buf, str + i * 2, 2);
-		buf[2] = 0;
-		ret = kstrtou8(buf, 16, &(*val)[i]);
-		if (ret)
-			return ret;
-	}
-	return 0;
-}
+#include "lora.h"
 
 #define RN2483_CMD_TIMEOUT HZ
 
