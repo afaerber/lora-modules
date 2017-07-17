925322a3c291adf56ba32d9059f9fc12ff94f747
Andreas Färber <afaerber@suse.de>
2017-07-17 02:57:16 +0200

    sx1276: Implement FIFO writes
    Add burst write helper.
    Fix Fr vs. Frf inconsistency.
    
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- sx1276.c
+++ sx1276.c
@@ -17,15 +17,18 @@
 #include "af_lora.h"
 #include "lora.h"
 
-#define REG_OPMODE	0x01
-#define REG_FR_MSB	0x06
-#define REG_FR_MID	0x07
-#define REG_FR_LSB	0x08
-#define REG_VERSION	0x42
+#define REG_FIFO			0x00
+#define REG_OPMODE			0x01
+#define REG_FRF_MSB			0x06
+#define REG_FRF_MID			0x07
+#define REG_FRF_LSB			0x08
+#define LORA_REG_FIFO_ADDR_PTR		0x0d
+#define LORA_REG_FIFO_TX_BASE_ADDR	0x0e
+#define REG_VERSION			0x42
 
 #define REG_OPMODE_LONG_RANGE_MODE		BIT(7)
 #define REG_OPMODE_LOW_FREQUENCY_MODE_ON	BIT(3)
-#define REG_OPMODE_MODE_MASK			(0x7 << 0)
+#define REG_OPMODE_MODE_MASK			GENMASK(2, 0)
 #define REG_OPMODE_MODE_SLEEP			(0x0 << 0)
 #define REG_OPMODE_MODE_STDBY			(0x1 << 0)
 #define REG_OPMODE_MODE_TX			(0x3 << 0)
@@ -34,6 +37,7 @@
 
 struct sx1276_priv {
 	struct lora_priv lora;
+	size_t fifosize;
 };
 
 static int sx1276_read_single(struct spi_device *spi, u8 reg, u8 *val)
@@ -46,13 +50,39 @@ static int sx1276_write_single(struct spi_device *spi, u8 reg, u8 val)
 {
 	u8 buf[2];
 
-	buf[0] = reg | 0x80;
+	buf[0] = reg | BIT(7);
 	buf[1] = val;
-	return spi_write_then_read(spi, buf, 2, NULL, 0);
+	return spi_write(spi, buf, 2);
+}
+
+static int sx1276_write_burst(struct spi_device *spi, u8 reg, size_t len, void *val)
+{
+	u8 buf = reg | BIT(7);
+	struct spi_transfer xfers[2] = {
+		[0] = {
+			.tx_buf = &buf,
+			.len = 1,
+		},
+		[1] = {
+			.tx_buf = val,
+			.len = len,
+		},
+	};
+
+	return spi_sync_transfer(spi, xfers, 2);
+}
+
+static int sx1276_write_fifo(struct spi_device *spi, size_t len, void *val)
+{
+	return sx1276_write_burst(spi, REG_FIFO, len, val);
 }
 
 static netdev_tx_t sx1276_loradev_start_xmit(struct sk_buff *skb, struct net_device *netdev)
 {
+	struct spi_device *spi = to_spi_device(netdev->dev.parent);
+	u8 addr;
+	int ret;
+
 	if (skb->protocol != htons(ETH_P_LORA)) {
 		kfree_skb(skb);
 		netdev->stats.tx_dropped++;
@@ -60,6 +90,24 @@ static netdev_tx_t sx1276_loradev_start_xmit(struct sk_buff *skb, struct net_dev
 	}
 
 	netif_stop_queue(netdev);
+
+	ret = sx1276_read_single(spi, LORA_REG_FIFO_TX_BASE_ADDR, &addr);
+	if (ret < 0) {
+		netdev->stats.tx_dropped++;
+		return NETDEV_TX_OK;
+	}
+
+	ret = sx1276_write_single(spi, LORA_REG_FIFO_ADDR_PTR, addr);
+	if (ret < 0) {
+		netdev->stats.tx_dropped++;
+		return NETDEV_TX_OK;
+	}
+
+	ret = sx1276_write_fifo(spi, skb->data_len, skb->data);
+	if (ret < 0) {
+		netdev->stats.tx_dropped++;
+		return NETDEV_TX_OK;
+	}
 
 	/* TODO */
 	return NETDEV_TX_OK;
@@ -216,11 +264,11 @@ static int sx1276_probe(struct spi_device *spi)
 	freq_band = freq_band / freq_xosc * (2 << 19);
 	dev_dbg(&spi->dev, "FRF = %u", freq_band);
 
-	ret = sx1276_write_single(spi, REG_FR_MSB, freq_band >> 16);
+	ret = sx1276_write_single(spi, REG_FRF_MSB, freq_band >> 16);
 	if (!ret)
-		ret = sx1276_write_single(spi, REG_FR_MID, freq_band >> 8);
+		ret = sx1276_write_single(spi, REG_FRF_MID, freq_band >> 8);
 	if (!ret)
-		ret = sx1276_write_single(spi, REG_FR_LSB, freq_band);
+		ret = sx1276_write_single(spi, REG_FRF_LSB, freq_band);
 	if (ret) {
 		dev_err(&spi->dev, "failed writing frequency (%d)", ret);
 		return ret;
