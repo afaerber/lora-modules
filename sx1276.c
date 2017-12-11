e767e8ef7072099caa345234b3ff4941e9c92499
Andreas Färber <afaerber@suse.de>
2017-12-11 03:15:35 +0100

    sx1276: Try falling edge
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- sx1276.c
+++ sx1276.c
@@ -307,7 +307,7 @@ static int sx1276_loradev_open(struct net_device *netdev)
 			netdev_warn(netdev, "Failed to obtain interrupt for DIO0 (%d)\n", irq);
 		else {
 			netdev_info(netdev, "Succeeded in obtaining interrupt for DIO0: %d\n", irq);
-			ret = request_threaded_irq(irq, NULL, sx1276_dio_interrupt, IRQF_ONESHOT | IRQF_TRIGGER_RISING, netdev->name, netdev);
+			ret = request_threaded_irq(irq, NULL, sx1276_dio_interrupt, IRQF_ONESHOT | IRQF_TRIGGER_FALLING, netdev->name, netdev);
 			if (ret) {
 				netdev_err(netdev, "Failed to request interrupt for DIO0 (%d)\n", ret);
 				goto err_irq;
