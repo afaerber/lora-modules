b09c7978baf3cbbd844406e6d3e0e25322a1f937
Andreas Färber <afaerber@suse.de>
2018-07-13 04:28:37 +0200

    Handle lora-ting01m module
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- load.sh
+++ load.sh
@@ -6,6 +6,7 @@ rmmod lora-rn2483
 rmmod lora-wimod
 rmmod lora-usi
 rmmod lora-rak811
+rmmod lora-ting01m
 rmmod lora-dev
 rmmod lora
 
@@ -20,5 +21,6 @@ insmod ${BDIR}/drivers/net/lora/lora-rn2483.ko dyndbg
 insmod ${BDIR}/drivers/net/lora/lora-wimod.ko dyndbg
 insmod ${BDIR}/drivers/net/lora/lora-usi.ko dyndbg
 insmod ${BDIR}/drivers/net/lora/lora-rak811.ko dyndbg
+insmod ${BDIR}/drivers/net/lora/lora-ting01m.ko dyndbg
 insmod ${BDIR}/drivers/net/lora/lora-sx1257.ko dyndbg
 insmod ${BDIR}/drivers/net/lora/lora-sx1301.ko dyndbg
