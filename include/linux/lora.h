0eb518b512f2052c2b2294176586ce9817581e57
Andreas Färber <afaerber@suse.de>
2018-12-18 03:59:29 +0100

    Add additional ETH_P_ constants
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- include/linux/lora.h
+++ include/linux/lora.h
@@ -5,4 +5,8 @@
 
 #define ARPHRD_LORA 827
 
-#define ETH_P_LORA 0x00FA
+#define ETH_P_LORA	0x00FA
+#define ETH_P_LORAWAN	0x80FB
+#define ETH_P_OOK	0x80FC
+#define ETH_P_FSK	0x80FD
+#define ETH_P_FLRC	0x80FE
