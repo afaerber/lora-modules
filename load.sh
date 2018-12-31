465aedf0ea7d9295f93dd064310f0f2d740e6f4c
Andreas Färber <afaerber@suse.de>
2018-12-31 12:26:52 +0100

    Update to sx130x driver
    sx1301 was renamed to sx130x.
    
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- load.sh
+++ load.sh
@@ -2,6 +2,7 @@
 
 rmmod lora-sx125x
 rmmod lora-sx1301
+rmmod lora-sx130x
 rmmod lora-sx1257
 rmmod lora-sx128x
 rmmod lora-sx1276
@@ -35,5 +36,5 @@ insmod ${BDIR}/drivers/net/lora/lora-mm002.ko dyndbg
 insmod ${BDIR}/drivers/net/lora/lora-rf1276ts.ko dyndbg
 insmod ${BDIR}/drivers/net/lora/lora-sx127x.ko dyndbg
 insmod ${BDIR}/drivers/net/lora/lora-sx128x.ko dyndbg
-insmod ${BDIR}/drivers/net/lora/lora-sx1301.ko dyndbg
+insmod ${BDIR}/drivers/net/lora/lora-sx130x.ko dyndbg
 insmod ${BDIR}/drivers/net/lora/lora-sx125x.ko dyndbg
