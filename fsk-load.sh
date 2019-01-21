4920472fc1f4728766afc8b56709d9876962b601
Andreas Färber <afaerber@suse.de>
2019-01-21 03:13:03 +0100

    Load si443x module, too
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- fsk-load.sh
+++ fsk-load.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 rmmod fsk-nrf24l01p
+rmmod fsk-si443x
 
 set -e
 
 BDIR=linux
 
 insmod ${BDIR}/drivers/net/fsk/fsk-nrf24l01p.ko dyndbg
+insmod ${BDIR}/drivers/net/fsk/fsk-si443x.ko dyndbg
